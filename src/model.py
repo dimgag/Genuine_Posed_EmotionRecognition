029e26f36b1808b2d102aca0253d93b77f3bfa95
dimgag <gagatsis94@gmail.com>
2022-11-30 17:23:03 +0100

    dlt notebookk

--- src/model.py
+++ src/model.py
@@ -1,41 +1,56 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torchvision import models
 
-# Base model
-class Net(nn.Module):
+class VGGFace(nn.Module):
     def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 6, 5)
-        self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16*5*5, 120)
-        self.fc2 = nn.Linear(120, 84)
-        self.fc3 = nn.Linear(84, 10)
+        super(VGGFace, self).__init__()
+        self.features = models.vgg16(pretrained=True).features
+        self.classifier = nn.Sequential(
+            nn.Linear(25088, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 2622),
+        )
 
     def forward(self, x):
-        x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2))
-        x = F.max_pool2d(F.relu(self.conv2(x)), 2)
-        x = x.view(-1, self.num_flat_features(x))
-        x = F.relu(self.fc1(x))
-        x = F.relu(self.fc2(x))
-        x = self.fc3(x)
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
         return x
 
-    def num_flat_features(self, x):
-        size = x.size()[1:]  # all dimensions except the batch dimension
-        num_features = 1
-        for s in size:
-            num_features *= s
-        return num_features
+vggface = VGGFace()
+print(vggface)
 
-net = Net()
-print(net)
-
-# Load resnet50 pretrained model 
+# Load VGGface2
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from torchvision import models
-resnet50 = models.resnet50(pretrained=True)
-print(resnet50)
 
-# Summarize the model
-from torchsummary import summary
-summary(resnet50, (3, 224, 224))
+class VGGFace2(nn.Module):
+    def __init__(self):
+        super(VGGFace2, self).__init__()
+        self.features = models.vgg16(pretrained=True).features
+        self.classifier = nn.Sequential(
+            nn.Linear(25088, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 4096),
+            nn.ReLU(True),
+            nn.Dropout(),
+            nn.Linear(4096, 8631),
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = x.view(x.size(0), -1)
+        x = self.classifier(x)
+        return x
+
+vggface2 = VGGFace2()
+print(vggface2)
