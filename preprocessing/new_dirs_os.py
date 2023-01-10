91a0e7ed9c1d90b95a7ff78aa79715e5373ff2a3
dimgag <gagatsis94@gmail.com>
2023-01-10 12:36:52 +0200

    add code

--- preprocessing/new_dirs_os.py
+++ preprocessing/new_dirs_os.py
@@ -1,5 +1,6 @@
 import os 
 import shutil
+import cv2 
 
 data_dir = "data/SASE-FE/FakeTrue_DB"
 persons = os.listdir(data_dir)
@@ -9,9 +10,19 @@ emotions = ['real_surprise','real_angry','real_happy','real_sad','real_disgust',
 
 
 
+from preprocessing_utils import get_files_paths
+
+r, r_subdir, r_file = get_files_paths(data_dir)
+
+print("\nWHAT is r", r[1])
+print("\nWHAT is r_subdir", r_subdir[1])
+print("\nWHAT is r_file", r_file[1])
+
 # List files dirs
+# print(os.listdir(data_dir))
 
 
+'''
 if ".DS_Store" in persons:
     persons.remove(".DS_Store")
 
@@ -41,6 +52,41 @@ for emotion in emotions:
 
 
 
+# Make Frames directory 
+if not os.path.exists("data/frames"):
+    os.mkdir("data/frames")
+
+# make persons directories in frames
+for person in persons:
+    if not os.path.exists("data/frames/" + person):
+        os.mkdir("data/frames/" + person)
+
+
+
+
+# Generate the frames from videos and move them to frames directories 
+for person in persons:
+    for emotion in emotions:
+        videos = os.listdir(data_dir + "/" + person + "/" + emotion)
+        for video in videos:
+            if ".DS_Store" in video:
+                videos.remove(".DS_Store")
+        for video in videos:
+            os.system("ffmpeg -i " + data_dir + "/" + person + "/" + emotion + "/" + video + " -vf fps=1 data/frames/" + person + "/" + video[:-4] + "_%d.jpg")
+    print("Frames Generated")
+
+
+
+
+
+# Crop the frames
+
+
+
+
+
+
+
 # for first 40 persons 
 for person in persons[:40]:
     for emotion in emotions:
@@ -64,4 +110,4 @@ for person in persons[40:]:
     print("Test Directory Created")
 
 
-# 
+# '''
