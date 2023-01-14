93d32d38e42ca3129fb60d87dc49345057e0ffd3
dimgag <gagatsis94@gmail.com>
2023-01-14 13:18:22 +0100

    add data aumentation

--- src/data.py
+++ src/data.py
@@ -31,7 +31,10 @@ if '.DS_Store' in test_images:
 def get_train_transform(image_size):
   train_transform = transforms.Compose([
       transforms.Resize((image_size, image_size)),
-      transforms.ToTensor()
+      transforms.RandomHorizontalFlip(p=0.5),
+      transforms.RandomRotation(35),
+      transforms.ToTensor(),
+      transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
   return train_transform
 
@@ -39,7 +42,8 @@ def get_train_transform(image_size):
 def get_test_transform(image_size):
     test_transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),
-        transforms.ToTensor()
+        transforms.ToTensor(),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
     return test_transform
 
