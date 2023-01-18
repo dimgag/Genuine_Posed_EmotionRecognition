9247329d07380eb7d6a1a3a979c23c405a372e8a
dimgag <gagatsis94@gmail.com>
2023-01-18 11:04:06 +0100

    add LR scheduler

--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as F
-
+from torch.optim.lr_scheduler import StepLR, ReduceLROnPlateau
 
 from utils import save_model, save_plots, get_model_params, ConfusionMatrix
 from data import get_datasets, get_data_loaders
@@ -56,6 +56,9 @@ if __name__ == '__main__':
 
     optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
+    # Scheduler
+    scheduler = ReduceLROnPlateau(optimizer, mode='max', factor=0.5, patience=2, verbose=True)
+
     # 4. Train the model
     # Lists to keep track of the loss and accuracy.
     train_loss, valid_loss = [], []
@@ -81,7 +84,11 @@ if __name__ == '__main__':
         valid_loss.append(valid_epoch_loss)
         train_acc.append(train_epoch_acc)
         valid_acc.append(valid_epoch_acc)
-
+        
+        # Update the learning rate.
+        scheduler.step(valid_epoch_acc)
+        
+        # Print the loss and accuracy for the epoch.
         print(f"Training loss: {train_epoch_loss:.3f}, training acc: {train_epoch_acc:.3f}")
         print(f"Validation loss: {valid_epoch_loss:.3f}, validation acc: {valid_epoch_acc:.3f}")
         print('-'*50)
