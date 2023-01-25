c277856eed74a400e64ad966e389b4f82af5b3d4
dimgag <gagatsis94@gmail.com>
2023-01-25 21:56:34 +0100

    model change

--- src-mtl/models.py
+++ src-mtl/models.py
@@ -37,11 +37,11 @@ class ChimeraNet(nn.Module):
     def __init__(self):
         super().__init__()
         self.net = InceptionResnetV1(pretrained='vggface2')
-        self.n_features = self.net.fc.in_features
+        # self.n_features = self.net.fc.in_features
 
         self.net.fc = nn.Identity()
-        self.net.fc1 = nn.Sequential(OrderedDict([('linear', nn.Linear(self.n_features,self.n_features)),('relu1', nn.ReLU()),('final', nn.Linear(self.n_features, 1))]))
-        self.net.fc2 = nn.Sequential(OrderedDict([('linear', nn.Linear(self.n_features,self.n_features)),('relu1', nn.ReLU()),('final', nn.Linear(self.n_features, 6))]))
+        self.net.fc1 = nn.Sequential(OrderedDict([('linear', nn.Linear(512,256)),('relu1', nn.ReLU()),('final', nn.Linear(256, 1))]))
+        self.net.fc2 = nn.Sequential(OrderedDict([('linear', nn.Linear(512,256)),('relu1', nn.ReLU()),('final', nn.Linear(256, 6))]))
         
     def forward(self, x):
         real_fake_head = self.net.fc1(self.net(x))
