cae871912f25f254cdfb96757542b4b51299cf2a
dimgag <gagatsis94@gmail.com>
2023-02-18 20:02:46 +0100

    add loss calc in evaluate file

--- src-mtl/evaluate.py
+++ src-mtl/evaluate.py
@@ -35,14 +35,33 @@ def evaluate(model, testloader):
 
             # Forward pass
             real_fake_output, emotion_output = model(inputs)
-
+            # ------------------------------------------------------------
             # Calculate the Loss
             loss_1 = emotion_loss(emotion_output, emotion_label)
             loss_2 = real_fake_loss(real_fake_output, real_fake_label)
-            
-            loss = loss_1 + loss_2
+            # IDEA 1: TOTAL LOSS = SUM
+            # loss = loss_1 + loss_2
+            # total_training_loss += loss
+            # loss = loss_1 + loss_2
+            # total_validation_loss += loss
+            # ------------------------------------------------------------
+            # IDEA 2: TOTAL LOSS = PRECISION_1 * LOSS_1 + PRECISION_2 * LOSS_2
+            # Calculate precision for emotions
+            _, emo_preds = torch.max(emotion_output.data, 1)
+            emo_true_positives = (emo_preds == emotion_label).sum().item()
+            emo_total_predicted_positives = (emo_preds == emo_preds).sum().item()
+            precision_emotions = emo_true_positives / emo_total_predicted_positives
+            # Calculate precision for emotions and real/fake 
+            _, rf_preds = torch.max(real_fake_output.data, 1)
+            rf_true_positives = (rf_preds == real_fake_label).sum().item()
+            rf_total_predicted_positives = (rf_preds == rf_preds).sum().item()
+            precision_real_fake = rf_true_positives / rf_total_predicted_positives
+            # Calculation of combined loss
+            loss_emotions = precision_emotions * loss_1
+            loss_real_fake = precision_real_fake * loss_2
+            loss = loss_emotions + loss_real_fake
             total_validation_loss += loss
-
+            # ------------------------------------------------------------
             # Calculate Accuracy for Emotions
             _, emo_preds = torch.max(emotion_output.data, 1)
             emotion_validation_acc += (emo_preds == emotion_label).sum().item()
@@ -52,7 +71,6 @@ def evaluate(model, testloader):
             # Calculate Overall Accuracy
             overall_validation_acc += (rf_preds == real_fake_label).sum().item()
             overall_validation_acc += (emo_preds == emotion_label).sum().item()
-
 
         epoch_loss = total_validation_loss / counter
         epoch_acc_emotion = 100. * (emotion_validation_acc / len(testloader.dataset))
