b33a378af7a6c9c4bea6d43d75ef5f6014bf2a00
dimgag <gagatsis94@gmail.com>
2023-02-23 12:08:40 +0000

    commid change

--- src-mtl/evaluate.py
+++ src-mtl/evaluate.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
     torch.cuda.empty_cache()
 
     # Load the model.pth - change the path to the model you want to evaluate
-    path = 'experiments/exp1111/model.pth'
+    path = 'experiments/exp-test-loss/model.pth'
 
     # Device configuration
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
