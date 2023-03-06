b1f3d2ce12ed4f9a6a16ed0c6f898d4bfd10ba4c
dimgag <gagatsis94@gmail.com>
2023-03-06 12:35:09 +0000

    dlib face landmarks

--- frame-seq/facial_landmarks.py
+++ frame-seq/facial_landmarks.py
@@ -1,13 +1,19 @@
 import dlib
 import cv2
+import subprocess
+
+# Run this "rm -rf `find -type d -name .ipynb_checkpoints`" in terminal to avoid .ipynb_checkpoints
+# subprocess.run(["rm", "-rf", "`find", "-type", "d", "-name", ".ipynb_checkpoints`"])
+# subprocess.run(["rm", "-rf", "`find", "-type", "d", "-name", ".DS_Store`"])
+
 
 # Initialize the facial landmark detector
 detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor('/Users/dim__gag/git/Genuine_Posed_EmotionRecognition/frame-seq/shape_predictor_68_face_landmarks.dat')
-# predictor = dlib.shape_predictor('/Users/dim__gag/git/Genuine_Posed_EmotionRecognition/frame-seq/shape_predictor_68_face_landmarks_GTX.dat')
+predictor = dlib.shape_predictor('frame-seq/shape_predictor_68_face_landmarks.dat')
+
 
 # Load an image
-img = cv2.imread('/Users/dim__gag/Desktop/Image.jpeg')
+img = cv2.imread('Image.jpeg')
 
 # Convert the image to grayscale
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -27,11 +33,11 @@ for face in faces:
         cv2.circle(img, (x, y), 2, (0, 255, 0), -1)
 
 # Display the image with facial landmarks
-cv2.imshow('Facial Landmarks', img)
-cv2.imwrite('/Users/dim__gag/Desktop/snowbaby.jpg', img)
+# cv2.imshow('Facial Landmarks', img)
+cv2.imwrite('snowbaby.jpg', img)
 
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
 
 
 
