06e830d40a1b73651bd544a1b4becc8b85cc749d
dimgag <gagatsis94@gmail.com>
2023-03-12 18:37:49 +0100

    writing code for mtl transformer

---
+++ src-mtl-transformer/dataset.py
@@ -0,0 +1,114 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from PIL import Image
+import torch
+import torch.nn as nn
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+
+
+class MTL_VideoDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+
+        # Get the list of class folders
+        self.class_folders = os.listdir(self.root_dir)
+
+        # Initialize the lists of classes for each task
+        self.rf_classes = ['fake', 'real']
+        self.emo_classes = ['angry', 'contempt', 'disgust', 'happy', 'sad', 'surprise']
+
+    def __len__(self):
+        # Count the total number of videos in the dataset
+        return sum([len(os.listdir(os.path.join(self.root_dir, c))) for c in self.class_folders])
+
+    def __getitem__(self, idx):
+        # Select a random video and its corresponding class folder
+        video_folder_idx = np.random.randint(len(self.class_folders))
+        video_folder = self.class_folders[video_folder_idx]
+        video_path = os.path.join(self.root_dir, video_folder)
+        video_files = os.listdir(video_path)
+        video_file_idx = np.random.randint(len(video_files))
+        video_file = video_files[video_file_idx]
+
+        # Load the video frames
+        cap = cv2.VideoCapture(os.path.join(video_path, video_file))
+        frames = []
+        
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+
+            face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_alt2.xml")
+            face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_alt2.xml')
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            gray = cv2.GaussianBlur(frame, (25,25), 0)
+            face = face_cascade.detectMultiScale(gray, 1.1, 4)
+            for (x,y,w,h) in face:
+                frame = frame[y:y+h, x:x+w]
+                frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+
+            if self.transform:
+                frame = self.transform(frame)
+                
+            frames.append(frame)
+            
+
+
+        # Get the labels for the two tasks
+        rf_label = self.rf_classes.index(video_folder.split("_")[0])
+        emo_label = self.emo_classes.index(video_folder.split("_")[1])
+
+        frames = [torch.from_numpy(frame) for frame in frames]
+        return torch.stack(frames), rf_label, emo_label
+
+
+
+class MyTransform:
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, frame):
+        # Convert NumPy array to PIL Image
+        frame = Image.fromarray(frame)
+        # Apply transformations
+        frame = transforms.Resize(self.size)(frame)
+        
+        # Add more transformations here if needed
+        
+        # Convert PIL Image back to NumPy array
+        frame = np.array(frame)
+        return frame
+
+
+# test the code
+if __name__ == "__main__":
+    # define the data directory
+    data_dir = 'data_temporal/train_root'
+
+    # define the transform
+    transform = MyTransform((256, 256))
+
+    dataset = MTL_VideoDataset(data_dir, transform=transform)
+
+    # print(dataset[0][0].shape)
+
+    # Get an example output with labels
+    frames, label1, label2 = dataset[0]
+    # Print the labels
+    print(f"Label 1: {label1}")
+    print(f"Label 2: {label2}")
+
+
+    # plot the first 10 frames
+    fig, ax = plt.subplots(2, 5)
+    for i in range(10):
+        ax[i//5, i%5].imshow(frames[i])
+    plt.show()
+
+
+
--- src-mtl-transformer/main.py
+++ src-mtl-transformer/main.py
@@ -37,3 +37,134 @@ data_temporal /
 Where each folder contains videos.
 
 '''
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torch.utils.data import Dataset, DataLoader
+import os
+import cv2
+import numpy as np
+'''
+class VideoDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.classes = os.listdir(self.root_dir)
+
+    def __len__(self):
+        return sum([len(os.listdir(os.path.join(self.root_dir, c))) for c in self.classes])
+
+    def __getitem__(self, idx):
+        class_folders = os.listdir(self.root_dir)
+        class_idx = np.random.randint(0, len(class_folders))
+        class_folder = class_folders[class_idx]
+        class_videos = os.listdir(os.path.join(self.root_dir, class_folder))
+        video_idx = np.random.randint(0, len(class_videos))
+        video_path = os.path.join(self.root_dir, class_folder, class_videos[video_idx])
+        cap = cv2.VideoCapture(video_path)
+        frames = []
+        while True:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            if self.transform:
+                frame = self.transform(frame)
+            frames.append(frame)
+        label1 = class_idx
+        label2 = int(class_folder.split("_")[0] == "real")
+        return torch.stack(frames), label1, label2
+
+
+'''
+
+
+
+
+class MultiTaskTransformer(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim1, output_dim2, num_heads, num_layers):
+        super(MultiTaskTransformer, self).__init__()
+        self.d_model = input_dim
+        self.transformer = nn.TransformerEncoder(nn.TransformerEncoderLayer(input_dim, num_heads, hidden_dim), num_layers)
+        self.fc1 = nn.Linear(input_dim, output_dim1)
+        self.fc2 = nn.Linear(input_dim, output_dim2)
+
+    def forward(self, x):
+        x = self.transformer(x)
+        x1 = self.fc1(x)
+        x2 = self.fc2(x)
+        return x1, x2
+
+# example usage
+mtl_model = MultiTaskTransformer(3, 16, 6, 2, 3, 4)
+
+
+# input_tensor = torch.randn(10, 20, 3) # batch size x sequence length x input dimension
+# output1, output2 = mtl_model(input_tensor)
+# print(output1.shape, output2.shape)
+
+# Find trainable parameters
+total_params = sum(p.numel() for p in mtl_model.parameters())
+print(f'{total_params:,} total parameters.')
+total_trainable_params = sum(
+    p.numel() for p in mtl_model.parameters() if p.requires_grad)
+print(f'{total_trainable_params:,} training parameters.')
+
+
+
+
+'''
+# Hyperparameters
+input_dim = 3 # RGB channels
+hidden_dim = 128
+output_dim1 = 6 # 6 classes for task 1
+output_dim2 = 2 # 2 classes for task 2
+num_heads = 8
+num_layers = 4
+batch_size = 32
+lr = 0.001
+num_epochs = 10
+
+# Data
+train_transforms = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+train_dataset = VideoDataset("data_temporal/train_root", transform=train_transforms)
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+val_transforms = transforms.Compose([transforms.Resize((224, 224)), transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+val_dataset = VideoDataset("data_temporal/val_root", transform=val_transforms)
+val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
+
+# Model, loss and optimizer
+model = MultiTaskTransformer(input_dim, hidden_dim, output_dim1, output_dim2, num_heads, num_layers)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=lr)
+
+# Train the model
+for epoch in range(num_epochs):
+    running_loss1, running_loss2 = 0.0, 0.0
+    for inputs, labels1, labels2 in train_loader:
+        optimizer.zero_grad()
+        outputs1, outputs2 = model(inputs)
+        loss1 = criterion(outputs1, labels1)
+        loss2 = criterion(outputs2, labels2)
+        loss = loss1 + loss2
+        loss.backward()
+        optimizer.step()
+        running_loss1 += loss1.item()
+        running_loss2 += loss2.item()
+    print(f"Epoch {epoch+1}: Loss1={running_loss1/len(train_loader):.4f}, Loss2={running_loss2/len(train_loader):.4f}")
+    # Validate the model
+    correct1, total1, correct2, total2 = 0, 0, 0, 0
+    with torch.no_grad():
+        for inputs, labels1, labels2 in val_loader:
+            outputs1, outputs2 = model(inputs)
+            _, predicted1 = torch.max(outputs1.data, 1)
+            _, predicted2 = torch.max(outputs2.data, 1)
+            total1 += labels1.size(0)
+            total2 += labels2.size(0)
+            correct1 += (predicted1 == labels1).sum().item()
+            correct2 += (predicted2 == labels2).sum().item()
+    print(f"Accuracy1={correct1/total1:.4f}, Accuracy2={correct2/total2:.4f}")
+
+'''
