bf290be0c7ea52cddefb3192c0835e9ed86a2f5d
dimgag <gagatsis94@gmail.com>
2023-03-14 18:30:28 +0100

    add main file

---
+++ new_code/main.py
@@ -0,0 +1,137 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader, random_split
+from sklearn.model_selection import train_test_split
+from dataset import VideoDataset
+# from model import CRNN
+
+
+from dataset import VideoDataset, get_data_loaders
+
+# Set the device to use for training (GPU if available, else CPU)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+train_dataloader, val_dataloader = get_data_loaders('data_sequences/train_seq',
+                                                    'data_sequences/val_seq',
+                                                    seq_length=20,
+                                                    batch_size=32,
+                                                    num_workers=2)
+
+
+
+# define the model
+class CRNN(nn.Module):
+    def __init__(self, num_classes):
+        super(CRNN, self).__init__()
+        self.num_classes = num_classes
+        self.cnn = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1)),
+            nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=(2, 1), stride=(2, 1)),
+            nn.Conv2d(512, 512, kernel_size=2, stride=1, padding=0),
+            nn.BatchNorm2d(512),
+            nn.ReLU(inplace=True)
+        )
+        self.rnn = nn.Sequential(
+            nn.LSTM(512, 256, bidirectional=True, batch_first=True),
+            nn.LSTM(512, 256, bidirectional=True, batch_first=True)
+        )
+        self.fc = nn.Linear(512, num_classes)
+
+    def forward(self, x):
+        # CNN
+        x = self.cnn(x)
+        # Permute batch_size and seq_len
+        x = x.permute(0, 3, 1, 2)
+        # Reshape to (batch_size*seq_len, hidden_size)
+        batch_size = x.size(0)
+        x = x.reshape(batch_size, x.size(1), -1)
+        # RNN
+        x, _ = self.rnn(x)
+        # Reshape to (batch_size, seq_len, hidden_size)
+        x = x.reshape(batch_size, -1, x.size(2))
+        # FC
+        x = self.fc(x)
+        return x
+    
+# Define the network
+class VideoClassifier(nn.Module):
+    def __init__(self):
+        super(VideoClassifier, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.fc1 = nn.Linear(128 * 8 * 8, 512)
+        self.fc2 = nn.Linear(512, 6)
+
+    def forward(self, x):
+        x = self.pool(torch.relu(self.conv1(x)))
+        x = self.pool(torch.relu(self.conv2(x)))
+        x = self.pool(torch.relu(self.conv3(x)))
+        x = x.view(-1, 128 * 8 * 8)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+# Define the model
+model = CRNN(num_classes=12).to(device)
+
+# Define the loss function and optimizer
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
+
+# Train the model
+num_epochs = 10
+for epoch in range(num_epochs):
+    running_loss = 0.0
+    for i, (inputs, labels) in enumerate(train_dataloader):
+        inputs = inputs.to(device)
+        labels = labels.to(device)
+
+        optimizer.zero_grad()
+
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    # Evaluate the model on the validation set
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for inputs, labels in val_dataloader:
+            inputs = inputs.to(device)
+            labels = labels.to(device)
+
+            outputs = model(inputs)
+            _, predicted = torch.max(outputs.data, 1)
+
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('Epoch [%d], Loss: %.4f, Validation Accuracy: %.4f' %
+          (epoch+1, running_loss/len(train_dataloader), correct/total))
+
