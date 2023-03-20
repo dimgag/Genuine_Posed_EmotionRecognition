aa9e672f1f2ebc0b632874646e007ade1f222378
dimgag <gagatsis94@gmail.com>
2023-03-20 23:12:59 +0100

    add model ideas to try

--- src-seq/models.py
+++ src-seq/models.py
@@ -138,3 +138,124 @@ class EmotionRecognitionModel_Bigger(nn.Module):
         x = self.relu4(x)
         x = self.fc2(x)
         return x
+    
+
+
+
+
+# GRU Model
+
+class GRU(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(GRU, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.gru = nn.GRU(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        out, _ = self.gru(x, h0)
+        out = self.fc(out[:, -1, :])
+        return out
+    
+
+
+# LSTM Model
+
+class LSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(LSTM, self).__init__()
+        self.num_layers = num_layers
+        self.hidden_size = hidden_size
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
+        self.fc = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(device)
+        out, _ = self.lstm(x, (h0, c0))
+        out = self.fc(out[:, -1, :])
+        return out
+    
+
+# AKMNet-Micro-Expression
+
+class AKMNet(nn.Module):
+    def __init__(self, num_classes):
+        super(AKMNet, self).__init__()
+        self.conv1 = nn.Conv3d(3, 32, kernel_size=(3, 5, 5), stride=(1, 2, 2), padding=(1, 2, 2))
+        self.bn1 = nn.BatchNorm3d(32)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.pool1 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        
+        self.conv2 = nn.Conv3d(32, 64, kernel_size=(3, 5, 5), stride=(1, 1, 1), padding=(1, 2, 2))
+        self.bn2 = nn.BatchNorm3d(64)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.pool2 = nn.MaxPool3d(kernel_size=(1, 2, 2), stride=(1, 2, 2))
+        
+        self.conv3 = nn.Conv3d(64, 128, kernel_size=(3, 5, 5), stride=(1, 1, 1), padding=(1, 2, 2))
+        self.bn3 = nn.BatchNorm3d(128)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.pool3 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        
+        self.conv4 = nn.Conv3d(128, 256, kernel_size=(3, 5, 5), stride=(1, 1, 1), padding=(1, 2, 2))
+        self.bn4 = nn.BatchNorm3d(256)
+        self.relu4 = nn.ReLU(inplace=True)
+        self.pool4 = nn.MaxPool3d(kernel_size=(2, 2, 2), stride=(2, 2, 2))
+        
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(256*2*2*2, 1024)
+        self.relu5 = nn.ReLU(inplace=True)
+        self.fc2 = nn.Linear(1024, num_classes)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu1(x)
+        x = self.pool1(x)
+
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.relu2(x)
+        x = self.pool2(x)
+
+        x = self.conv3(x)
+        x = self.bn3(x)
+        x = self.relu3(x)
+        x = self.pool3(x)
+
+        x = self.conv4(x)
+        x = self.bn4(x)
+        x = self.relu4(x)
+        x = self.pool4(x)
+
+        x = self.flatten(x)
+        x = self.fc1(x)
+        x = self.relu5(x)
+        x = self.fc2(x)
+
+        return x
+    
+
+# X3D Model from X3D networks pretrained on the Kinetics 400 dataset 
+# PyTorchVideo: https://pytorch.org/hub/facebookresearch_pytorchvideo_x3d/
+
+import torch
+# Choose the `x3d_s` model
+model_name = 'x3d_s'
+model = torch.hub.load('facebookresearch/pytorchvideo', model_name, pretrained=True)
+
+
+# 3D RESNET
+# Try this one: 3D RESNET https://pytorch.org/hub/facebookresearch_pytorchvideo_resnet/
+model = torch.hub.load('facebookresearch/pytorchvideo', 'slow_r50', pretrained=True)
+class X3D(nn.Module):
+    def __init__(self, num_classes):
+        super(X3D, self).__init__()
+        self.model = torch.hub.load('facebookresearch/pytorchvideo', 'slow_r50', pretrained=True)
+        self.model.fc = nn.Linear(2048, num_classes)
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
